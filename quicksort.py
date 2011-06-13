466ebbdb322dd217fc944c59ef4f09838b0fe32b
Valentin Haenel <valentin.haenel@gmx.de>
2011-06-13 19:17:25 +0200

    add a mergesort

--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,4 @@
-from random import randint, shuffle
+from random import randint, shuffle, choice
 
 def swap(array, i, j):
     array[i], array[j] = array[j], array[i]
@@ -24,8 +24,25 @@ def quicksort(array, low, high):
     quicksort(array, low, counter-1)
     quicksort(array, counter+1, high)
 
+def mergesort(array):
+    """ Stable mergesort."""
+    if len(array) <= 1:
+        return array
+    lower, upper, center = [], [], []
+    part = choice(array)
+    for i in array:
+        if i < part:
+            lower.append(i)
+        elif i > part:
+            upper.append(i)
+        else:
+            center.append(i)
+    return mergesort(lower) + center + mergesort(upper)
+
 array = range(0, 32)
 shuffle(array)
+print array
+print mergesort(array)
 print array
 quicksort(array, 0, len(array)-1)
 print array
